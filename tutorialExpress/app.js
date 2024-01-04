a8c30f6d3cc781e4a24a69be99114bbc051fa44e
NyanLinHtet1999 <andydufresne323@gmail.com>
2024-01-05 00:37:09 +0630

    post

--- tutorialExpress/app.js
+++ tutorialExpress/app.js
@@ -1,27 +1,42 @@
 const express = require("express")
 const app = express()
-const logger = require("./logger")
+
+app.use(express.static("./methods-public"))
+app.use(express.urlencoded({extended : true}))
+app.use(express.json())
+
+let {people} = require("./data")
 
 
-// req => middleware => res 
 
-app.use("/api",logger)//only effect include api route
-
-
-app.get("/",(req, res) => {
-     res.end("Home Page")
+app.get("/api/people", (req, res) => {
+    res.status(200).json({success : true, data : people})
 })
-app.get("/about",(req, res) => {
-     res.end("About Page")
+app.post("/api/people", (req, res) => {
+    const {name} = req.body
+    if(!name) {
+        return res.status(400).json({success : false ,msg : "Please provide name"})
+    }
+    console.log(name)
+    res.status(201).json({success : true, person : name})
 })
-app.get("/api/products",(req, res) => {
-     res.send("Products")
-})
-app.get("/api/items",(req, res) => {
-     res.send("Items")
+app.post("/api/postman/people", (req, res) => {
+    const {name} = req.body
+    if(!name) {
+        return res.status(400).json({success : false ,msg : "Please provide name"})
+    }
+    console.log(name)
+    res.status(201).json({success : true, data : [...people, name]})
 })
 
+app.post("/login", (req, res) => {
+    const {name} = req.body
+    if(name) {
+        return res.status(200).send(`Welcome ${name}`)
+    }
+    res.status(401).send('Please provide credentials')
+})
 
-app.listen(5000, logger,() => {
+ app.listen(5000, () => {
      console.log("server is listening")
-})
+ })
